b5ca379836155bf18586bf3e191a923f6fd05db8
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-26 21:01:17 +0800

    控制台切换为窗口，wWinMain方法入口

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,29 +1,12 @@
 // ConsoleApplication2.cpp : 定义控制台应用程序的入口点。
 //
-
 #include "stdafx.h"
 #include <windows.h>
-#include <tchar.h>
 
-#define _UNICODE
-#define UNICODE
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PWSTR szCmdLine, int CmdShow) {
 
-int _tmain(int argc, TCHAR *argv[]) {
-
-	PDWORD cChars = NULL;
-	HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	if (std == INVALID_HANDLE_VALUE) {
-		_tprintf(L"Cannot retrieve standard output handle\n (%d)",
-			GetLastError());
-	}
-
-	if (argv[1]) {
-
-		WriteConsole(std, argv[1], _tcslen(argv[1]), cChars, NULL);
-	}
-
-	CloseHandle(std);
+	MessageBoxW(NULL, szCmdLine, L"Title", MB_OK);
 
 	return 0;
 }
