842900a321832d549194ac9c8e0f41ae256a2bda
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-26 20:18:31 +0800

    wmain方法入口

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,12 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
+#include <wchar.h>
 
+int wmain(int argc, wchar_t **argv) {
 
-int main()
-{
-	puts("This is a classic C program.");
-	getchar();
+	PDWORD cChars = NULL;
+	HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (std == INVALID_HANDLE_VALUE) {
+		wprintf(L"Cannot retrieve standard output handle\n (%d)",
+			GetLastError());
+	}
+
+	if (argv[1]) {
+
+		WriteConsoleW(std, argv[1], wcslen(argv[1]), cChars, NULL);
+	}	
+
+	CloseHandle(std);
 	return 0;
 }
 
