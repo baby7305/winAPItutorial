0152b7d0d704a721214b54350ef6fd24f7d49866
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 23:31:32 +0800

    下拉框测试

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -3,30 +3,32 @@
 #include "stdafx.h"
 #include <windows.h>
 
-#define ID_EDIT 1
-#define ID_BUTTON 2
-
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+HINSTANCE g_hinst;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	PWSTR lpCmdLine, int nCmdShow) {
 
+	HWND hwnd;
 	MSG  msg;
 	WNDCLASSW wc = { 0 };
-	wc.lpszClassName = L"Edit control";
+	wc.lpszClassName = L"Application";
 	wc.hInstance = hInstance;
 	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
 	wc.lpfnWndProc = WndProc;
 	wc.hCursor = LoadCursor(0, IDC_ARROW);
 
+	g_hinst = hInstance;
+
 	RegisterClassW(&wc);
-	CreateWindowW(wc.lpszClassName, L"Edit control",
+	hwnd = CreateWindowW(wc.lpszClassName, L"Combo box",
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		220, 220, 280, 200, 0, 0, hInstance, 0);
+		100, 100, 270, 170, 0, 0, hInstance, 0);
+
 
 	while (GetMessage(&msg, NULL, 0, 0)) {
 
-		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
@@ -36,21 +38,30 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
 	WPARAM wParam, LPARAM lParam) {
 
-	static HWND hwndEdit;
-	HWND hwndButton;
+	static HWND hwndCombo, hwndStatic;
+	const wchar_t *items[] = { L"FreeBSD", L"OpenBSD",
+		L"NetBSD", L"Solaris", L"Arch" };
 
 	switch (msg) {
 
 	case WM_CREATE:
 
-		hwndEdit = CreateWindowW(L"Edit", NULL,
-			WS_CHILD | WS_VISIBLE | WS_BORDER,
-			50, 50, 150, 20, hwnd, (HMENU)ID_EDIT,
-			NULL, NULL);
+		hwndCombo = CreateWindowW(L"Combobox", NULL,
+			WS_CHILD | WS_VISIBLE | CBS_DROPDOWN,
+			10, 10, 120, 110, hwnd, NULL, g_hinst, NULL);
 
-		hwndButton = CreateWindowW(L"button", L"Set title",
-			WS_VISIBLE | WS_CHILD, 50, 100, 80, 25,
-			hwnd, (HMENU)ID_BUTTON, NULL, NULL);
+		CreateWindowW(L"Button", L"Drop down",
+			WS_CHILD | WS_VISIBLE,
+			150, 10, 90, 25, hwnd, (HMENU)1, g_hinst, NULL);
+
+		hwndStatic = CreateWindowW(L"Static", L"",
+			WS_CHILD | WS_VISIBLE,
+			150, 80, 90, 25, hwnd, NULL, g_hinst, NULL);
+
+		for (int i = 0; i < 4; i++) {
+
+			SendMessageW(hwndCombo, CB_ADDSTRING, 0, (LPARAM)items[i]);
+		}
 
 		break;
 
@@ -58,17 +69,18 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
 
 		if (HIWORD(wParam) == BN_CLICKED) {
 
-			int len = GetWindowTextLengthW(hwndEdit) + 1;
-			//wchar_t text[len];
-			wchar_t text[100];
-
-			GetWindowTextW(hwndEdit, text, len);
-			SetWindowTextW(hwnd, text);
+			SendMessage(hwndCombo, CB_SHOWDROPDOWN, (WPARAM)TRUE, 0);
 		}
 
+		if (HIWORD(wParam) == CBN_SELCHANGE) {
+
+			LRESULT sel = SendMessage(hwndCombo, CB_GETCURSEL, 0, 0);
+			SetWindowTextW(hwndStatic, items[sel]);
+		}
 		break;
 
 	case WM_DESTROY:
+
 		PostQuitMessage(0);
 		break;
 	}
