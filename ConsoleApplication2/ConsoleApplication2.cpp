4e6d6d43d66582edc16a169b5d84ebbccbae75b1
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 20:08:01 +0800

    格式化日期

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,11 +7,45 @@
 
 int wmain(void) {
 
-	SYSTEMTIME st = { 0 };
+	PDWORD cChars = NULL;
+	HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	GetLocalTime(&st);
+	if (std == INVALID_HANDLE_VALUE) {
+		wprintf(L"Cannot retrieve standard output handle %d\n",
+			GetLastError());
+		return 1;
+	}
 
-	wprintf(L"Today is: %d-%02d-%02d\n", st.wYear, st.wMonth, st.wDay);
+	SYSTEMTIME lt = { 0 };
+	GetLocalTime(&lt);
+
+	wchar_t buf[128] = { 0 };
+
+	int r = GetDateFormatEx(LOCALE_NAME_USER_DEFAULT, DATE_LONGDATE,
+		&lt, NULL, buf, sizeof(buf) / sizeof(buf[0]), NULL);
+
+	if (r == 0) {
+
+		wprintf(L"GetDateFormatEx function failed %d\n",
+			GetLastError());
+
+		CloseHandle(std);
+
+		return 1;
+	}
+
+	WriteConsoleW(std, buf, wcslen(buf), cChars, NULL);
+
+	r = CloseHandle(std);
+
+	if (r == 0) {
+
+		wprintf(L"Cannot close console handle %d\n",
+			GetLastError());
+		return 1;
+	}
+
+	CloseHandle(std);
 	getchar();
 	return 0;
 }
