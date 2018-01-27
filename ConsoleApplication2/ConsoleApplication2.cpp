0331b737695507de2bd6797191b1d34bbebe56e4
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 19:43:37 +0800

    创建窗口

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -3,11 +3,65 @@
 #include "stdafx.h"
 #include <windows.h>
 
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void CenterWindow(HWND);
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	PWSTR pCmdLine, int CmdShow) {
+	PWSTR pCmdLine, int nCmdShow) {
 
-	MessageBoxW(NULL, L"First Program", L"First", MB_OK);
+	MSG  msg;
+	WNDCLASSW wc = { 0 };
+	wc.lpszClassName = L"Center";
+	wc.hInstance = hInstance;
+	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+	wc.lpfnWndProc = WndProc;
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
 
-	return 0;
+	RegisterClassW(&wc);
+	CreateWindowW(wc.lpszClassName, L"Center",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		100, 100, 250, 150, 0, 0, hInstance, 0);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
+	WPARAM wParam, LPARAM lParam) {
+
+	switch (msg) {
+
+	case WM_CREATE:
+
+		CenterWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+void CenterWindow(HWND hwnd) {
+
+	RECT rc = { 0 };
+
+	GetWindowRect(hwnd, &rc);
+	int win_w = rc.right - rc.left;
+	int win_h = rc.bottom - rc.top;
+
+	int screen_w = GetSystemMetrics(SM_CXSCREEN);
+	int screen_h = GetSystemMetrics(SM_CYSCREEN);
+
+	SetWindowPos(hwnd, HWND_TOP, (screen_w - win_w) / 2,
+		(screen_h - win_h) / 2, 0, 0, SWP_NOSIZE);
 }
 
