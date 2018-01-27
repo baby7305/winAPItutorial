6c877dbbdfe70ee024faef4c8e7ce3b96ffb7762
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 22:23:55 +0800

    颜色选择器

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -4,32 +4,27 @@
 #include <windows.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK DialogProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK PanelProc(HWND, UINT, WPARAM, LPARAM);
 
-void CreateDialogBox(HWND);
-void RegisterDialogClass(HWND);
+void RegisterPanel(void);
+COLORREF ShowColorDialog(HWND);
 
-HINSTANCE ghInstance;
+COLORREF gColor = RGB(255, 255, 255);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	PWSTR pCmdLine, int nCmdShow) {
 
 	MSG  msg;
-	HWND hwnd;
-
 	WNDCLASSW wc = { 0 };
-
-	wc.lpszClassName = L"Window";
+	wc.lpszClassName = L"Color dialog box";
 	wc.hInstance = hInstance;
 	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
 	wc.lpfnWndProc = WndProc;
 
 	RegisterClassW(&wc);
-	hwnd = CreateWindowW(wc.lpszClassName, L"Window",
+	CreateWindowW(wc.lpszClassName, L"Color dialog box",
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		100, 100, 250, 150, NULL, NULL, hInstance, NULL);
-
-	ghInstance = hInstance;
+		150, 150, 250, 200, 0, 0, hInstance, 0);
 
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		DispatchMessage(&msg);
@@ -40,67 +35,86 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
+	static HWND hwndPanel;
+
 	switch (msg) {
 
 	case WM_CREATE:
-		RegisterDialogClass(hwnd);
-		CreateWindowW(L"button", L"Show dialog",
+	{
+		CreateWindowW(L"button", L"Color",
 			WS_VISIBLE | WS_CHILD,
-			20, 50, 95, 25, hwnd, (HMENU)1, NULL, NULL);
+			20, 30, 80, 25,
+			hwnd, (HMENU)1, NULL, NULL);
+
+		RegisterPanel();
+		hwndPanel = CreateWindowW(L"Panel", NULL,
+			WS_CHILD | WS_VISIBLE,
+			130, 30, 80, 80, hwnd, (HMENU)2, NULL, NULL);
 		break;
+	}
 
 	case WM_COMMAND:
-		CreateDialogBox(hwnd);
+	{
+		gColor = ShowColorDialog(hwnd);
+		InvalidateRect(hwndPanel, NULL, TRUE);
 		break;
+	}
 
 	case WM_DESTROY:
 	{
 		PostQuitMessage(0);
-		return 0;
+		break;
 	}
 	}
+
 	return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
-LRESULT CALLBACK DialogProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
+LRESULT CALLBACK PanelProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rect;
+
 	switch (msg) {
 
-	case WM_CREATE:
-		CreateWindowW(L"button", L"Ok",
-			WS_VISIBLE | WS_CHILD,
-			50, 50, 80, 25, hwnd, (HMENU)1, NULL, NULL);
+	case WM_PAINT:
+	{
+		GetClientRect(hwnd, &rect);
+		hdc = BeginPaint(hwnd, &ps);
+		SetBkColor(hdc, gColor);
+		ExtTextOut(hdc, 0, 0, ETO_OPAQUE, &rect, L"", 0, NULL);
+		EndPaint(hwnd, &ps);
 		break;
-
-	case WM_COMMAND:
-		DestroyWindow(hwnd);
-		break;
-
-	case WM_CLOSE:
-		DestroyWindow(hwnd);
-		break;
-
+	}
 	}
 
-	return (DefWindowProcW(hwnd, msg, wParam, lParam));
+	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-void RegisterDialogClass(HWND hwnd) {
 
-	WNDCLASSEXW wc = { 0 };
-	wc.cbSize = sizeof(WNDCLASSEXW);
-	wc.lpfnWndProc = (WNDPROC)DialogProc;
-	wc.hInstance = ghInstance;
-	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
-	wc.lpszClassName = L"DialogClass";
-	RegisterClassExW(&wc);
+COLORREF ShowColorDialog(HWND hwnd) {
 
+	CHOOSECOLOR cc;
+	static COLORREF crCustClr[16];
+
+	ZeroMemory(&cc, sizeof(cc));
+	cc.lStructSize = sizeof(cc);
+	cc.hwndOwner = hwnd;
+	cc.lpCustColors = (LPDWORD)crCustClr;
+	cc.rgbResult = RGB(0, 255, 0);
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	ChooseColor(&cc);
+
+	return cc.rgbResult;
 }
 
-void CreateDialogBox(HWND hwnd) {
+void RegisterPanel(void) {
 
-	CreateWindowExW(WS_EX_DLGMODALFRAME | WS_EX_TOPMOST, L"DialogClass", L"Dialog Box",
-		WS_VISIBLE | WS_SYSMENU | WS_CAPTION, 100, 100, 200, 150,
-		NULL, NULL, ghInstance, NULL);
+	WNDCLASSW rwc = { 0 };
+	rwc.lpszClassName = L"Panel";
+	rwc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	rwc.lpfnWndProc = PanelProc;
+	RegisterClassW(&rwc);
 }
 
