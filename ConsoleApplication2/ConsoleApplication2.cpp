df3e3798fe3e938ac527e6b862a745bb5847f0c2
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 20:05:12 +0800

    时间运算

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,14 +5,30 @@
 #include <windows.h>
 #include <wchar.h>
 
+#define NSECS 60*60*3
+
 int wmain(void) {
 
 	SYSTEMTIME st = { 0 };
+	FILETIME ft = { 0 };
 
-	GetSystemTime(&st);
+	GetLocalTime(&st);
 
-	wprintf(L"The UTC time is: %02d:%02d:%02d\n",
-		st.wHour, st.wMinute, st.wSecond);
+	wprintf(L"%02d/%02d/%04d %02d:%02d:%02d\n",
+		st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, st.wSecond);
+
+	SystemTimeToFileTime(&st, &ft);
+
+	ULARGE_INTEGER u = { 0 };
+
+	memcpy(&u, &ft, sizeof(u));
+	u.QuadPart += NSECS * 10000000LLU;
+	memcpy(&ft, &u, sizeof(ft));
+
+	FileTimeToSystemTime(&ft, &st);
+
+	wprintf(L"%02d/%02d/%04d %02d:%02d:%02d\n",
+		st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, st.wSecond);
 	getchar();
 	return 0;
 }
