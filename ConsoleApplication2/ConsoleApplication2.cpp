e057d0cf16514056ccfb63d7bf0d27f0c63809b2
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 20:47:19 +0800

    获取当前时区

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,51 +7,18 @@
 
 int wmain(void) {
 
-	SYSTEMTIME st1 = { 0 };
-	SYSTEMTIME st2 = { 0 };
-	FILETIME ft1 = { 0 };
-	FILETIME ft2 = { 0 };
+	TIME_ZONE_INFORMATION tzi = { 0 };
 
-	st1.wYear = 2015;
-	st1.wMonth = 4;
-	st1.wDay = 12;
+	int r = GetTimeZoneInformation(&tzi);
 
-	st2.wYear = 2015;
-	st2.wMonth = 5;
-	st2.wDay = 12;
+	if (r == TIME_ZONE_ID_INVALID) {
 
-	int r1 = SystemTimeToFileTime(&st1, &ft1);
-
-	if (r1 == 0) {
-
-		wprintf(L"Failed to convert system time to file time\n (%d)",
-			GetLastError());
+		wprintf(L"Failed to get time zone %d", GetLastError());
 		return 1;
 	}
 
-	int r2 = SystemTimeToFileTime(&st2, &ft2);
-
-	if (r2 == 0) {
-
-		wprintf(L"Failed to convert system time to file time\n (%d)",
-			GetLastError());
-		return 1;
-	}
-
-	short ct = CompareFileTime(&ft1, &ft2);
-
-	if (ct == -1) {
-
-		wprintf(L"4/12/2015 comes before 5/12/2015\n");
-	}
-	else if (ct == 0) {
-
-		wprintf(L"4/12/2015 is equal to 5/12/2015\n");
-	}
-	else if (ct == 1) {
-
-		wprintf(L"4/12/2015 comes after 5/12/2015\n");
-	}
+	wprintf(L"Time zone: %ls\n", tzi.StandardName);
+	wprintf(L"The bias is: %ld minutes\n", tzi.Bias);
 	getchar();
 	return 0;
 }
