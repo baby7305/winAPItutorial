b349b2e755d8d7ece0b4efd893f3e99a1fc74b70
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 20:19:16 +0800

    判断今天星期几

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -7,53 +7,13 @@
 
 int wmain(void) {
 
-	DWORD tc = GetTickCount();
+	SYSTEMTIME st = { 0 };
 
-	short seconds = tc / 1000 % 60;
-	short minutes = tc / 1000 / 60 % 60;
-	short hours = tc / 1000 / 60 / 60 % 24;
-	short days = tc / 1000 / 60 / 60 / 24 % 7;
-	short weeks = tc / 1000 / 60 / 60 / 24 / 7 % 52;
+	wchar_t *dn[] = { L"Sunday", L"Monday", L"Tuesday",
+		L"Wednesday", L"Thursday", L"Friday", L"Saturday" };
 
-	wprintf(L"Computer has been running for: ");
-
-	if (weeks > 0 && weeks != 1) {
-
-		wprintf(L"%hi weeks ", weeks);
-	}
-	else if (weeks == 1) {
-
-		wprintf(L"1 week ");
-	}
-
-	if (days > 0 && days != 1) {
-
-		wprintf(L"%hi days ", days);
-	}
-	else if (days == 1) {
-
-		wprintf(L"1 day ");
-	}
-
-	if (hours > 0 && hours != 1) {
-
-		wprintf(L"%hi hours ", hours);
-	}
-	else if (hours == 1) {
-
-		wprintf(L"1 hour ");
-	}
-
-	if (minutes > 0 && minutes != 1) {
-
-		wprintf(L"%hi minutes ", minutes);
-	}
-	else if (minutes == 1) {
-
-		wprintf(L"1 minute ");
-	}
-
-	wprintf(L"and %hi seconds\n", seconds);
+	GetLocalTime(&st);
+	wprintf(L"Today is %ls\n", dn[st.wDayOfWeek]);
 	getchar();
 	return 0;
 }
