1e486beb0973fbb69beeb3486387421dd4e5fa51
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 22:46:26 +0800

    按钮测试

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,22 +5,24 @@
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+#define ID_BEEP 1
+#define ID_QUIT 2
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	PWSTR lpCmdLine, int nCmdShow) {
 
 	MSG  msg;
 	WNDCLASSW wc = { 0 };
-	wc.lpszClassName = L"Static Control";
+	wc.lpszClassName = L"Buttons";
 	wc.hInstance = hInstance;
 	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
 	wc.lpfnWndProc = WndProc;
 	wc.hCursor = LoadCursor(0, IDC_ARROW);
 
-
 	RegisterClassW(&wc);
-	CreateWindowW(wc.lpszClassName, L"Criminal",
+	CreateWindowW(wc.lpszClassName, L"Buttons",
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		100, 100, 330, 270, 0, 0, hInstance, 0);
+		150, 150, 300, 200, 0, 0, hInstance, 0);
 
 	while (GetMessage(&msg, NULL, 0, 0)) {
 
@@ -34,25 +36,31 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
 	WPARAM wParam, LPARAM lParam) {
 
-	static wchar_t *lyrics = L"I know you told me I should stay away\n\
-I know you said he's just a dog astray\n\
-He is a bad boy with a tainted heart\n\
-And even I know this ain't smart\n\
-\n\
-But mama, I'm in love with a criminal\n\
-And this type of love isn't rational, it's physical\n\
-Mama, please don't cry, I will be alright\n\
-All reason aside, I just can't deny, love the guy\n\
-";
-
 	switch (msg) {
 
 	case WM_CREATE:
 
-		CreateWindowW(L"Static", lyrics,
-			WS_CHILD | WS_VISIBLE | SS_LEFT,
-			20, 20, 300, 230,
-			hwnd, (HMENU)1, NULL, NULL);
+		CreateWindowW(L"Button", L"Beep",
+			WS_VISIBLE | WS_CHILD,
+			20, 50, 80, 25, hwnd, (HMENU)ID_BEEP, NULL, NULL);
+
+		CreateWindowW(L"Button", L"Quit",
+			WS_VISIBLE | WS_CHILD,
+			120, 50, 80, 25, hwnd, (HMENU)ID_QUIT, NULL, NULL);
+		break;
+
+	case WM_COMMAND:
+
+		if (LOWORD(wParam) == ID_BEEP) {
+
+			MessageBeep(MB_OK);
+		}
+
+		if (LOWORD(wParam) == ID_QUIT) {
+
+			PostQuitMessage(0);
+		}
+
 		break;
 
 	case WM_DESTROY:
