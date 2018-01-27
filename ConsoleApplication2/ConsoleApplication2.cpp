18ef1f2770f4b675eeb7f7cc555f52792124929e
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 23:35:59 +0800

    列表框测试

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,81 +2,91 @@
 //
 #include "stdafx.h"
 #include <windows.h>
+#include <commctrl.h>
+#include <strsafe.h>
+
+#define IDC_LIST 1
+#define IDC_STATIC 2
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-HINSTANCE g_hinst;
+typedef struct {
+
+	wchar_t name[30];
+	wchar_t job[20];
+	int age;
+
+} Friends;
+
+Friends friends[] = {
+
+	{ L"Lucy", L"waitress", 18 },
+	{ L"Thomas", L"programmer", 25 },
+	{ L"George", L"police officer", 26 },
+	{ L"Michael", L"producer", 38 },
+	{ L"Jane", L"steward", 28 },
+};
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	PWSTR lpCmdLine, int nCmdShow) {
+	PWSTR pCmdLine, int nCmdShow) {
 
-	HWND hwnd;
 	MSG  msg;
 	WNDCLASSW wc = { 0 };
-	wc.lpszClassName = L"Application";
+	wc.lpszClassName = L"MyListBox";
 	wc.hInstance = hInstance;
 	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
 	wc.lpfnWndProc = WndProc;
 	wc.hCursor = LoadCursor(0, IDC_ARROW);
 
-	g_hinst = hInstance;
-
 	RegisterClassW(&wc);
-	hwnd = CreateWindowW(wc.lpszClassName, L"Combo box",
+	CreateWindowW(wc.lpszClassName, L"List Box",
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		100, 100, 270, 170, 0, 0, hInstance, 0);
-
+		100, 100, 340, 200, 0, 0, hInstance, 0);
 
 	while (GetMessage(&msg, NULL, 0, 0)) {
 
+		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
 	return (int)msg.wParam;
 }
 
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
 	WPARAM wParam, LPARAM lParam) {
 
-	static HWND hwndCombo, hwndStatic;
-	const wchar_t *items[] = { L"FreeBSD", L"OpenBSD",
-		L"NetBSD", L"Solaris", L"Arch" };
+	static HWND hwndList, hwndStatic;
+	wchar_t buf[128];
 
 	switch (msg) {
 
 	case WM_CREATE:
 
-		hwndCombo = CreateWindowW(L"Combobox", NULL,
-			WS_CHILD | WS_VISIBLE | CBS_DROPDOWN,
-			10, 10, 120, 110, hwnd, NULL, g_hinst, NULL);
+		hwndList = CreateWindowW(WC_LISTBOXW, NULL, WS_CHILD
+			| WS_VISIBLE | LBS_NOTIFY, 10, 10, 150, 120, hwnd,
+			(HMENU)IDC_LIST, NULL, NULL);
 
-		CreateWindowW(L"Button", L"Drop down",
-			WS_CHILD | WS_VISIBLE,
-			150, 10, 90, 25, hwnd, (HMENU)1, g_hinst, NULL);
+		hwndStatic = CreateWindowW(WC_STATICW, NULL, WS_CHILD | WS_VISIBLE,
+			200, 10, 120, 45, hwnd, (HMENU)IDC_STATIC, NULL, NULL);
 
-		hwndStatic = CreateWindowW(L"Static", L"",
-			WS_CHILD | WS_VISIBLE,
-			150, 80, 90, 25, hwnd, NULL, g_hinst, NULL);
-
-		for (int i = 0; i < 4; i++) {
-
-			SendMessageW(hwndCombo, CB_ADDSTRING, 0, (LPARAM)items[i]);
+		for (int i = 0; i < ARRAYSIZE(friends); i++) {
+			SendMessageW(hwndList, LB_ADDSTRING, 0, (LPARAM)friends[i].name);
 		}
 
 		break;
 
 	case WM_COMMAND:
 
-		if (HIWORD(wParam) == BN_CLICKED) {
-
-			SendMessage(hwndCombo, CB_SHOWDROPDOWN, (WPARAM)TRUE, 0);
+		if (LOWORD(wParam) == IDC_LIST) {
+			if (HIWORD(wParam) == LBN_SELCHANGE) {
+				int sel = (int)SendMessageW(hwndList, LB_GETCURSEL, 0, 0);
+				StringCbPrintfW(buf, ARRAYSIZE(buf), L"Job: %ls\nAge: %d",
+					friends[sel].job, friends[sel].age);
+				SetWindowTextW(hwndStatic, buf);
+			}
 		}
 
-		if (HIWORD(wParam) == CBN_SELCHANGE) {
-
-			LRESULT sel = SendMessage(hwndCombo, CB_GETCURSEL, 0, 0);
-			SetWindowTextW(hwndStatic, items[sel]);
-		}
 		break;
 
 	case WM_DESTROY:
@@ -85,6 +95,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg,
 		break;
 	}
 
-	return DefWindowProcW(hwnd, msg, wParam, lParam);
+	return (DefWindowProcW(hwnd, msg, wParam, lParam));
 }
 
