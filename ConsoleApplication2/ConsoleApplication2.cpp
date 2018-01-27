60e9edd8055c1ec96ba623b4e68cbef7b3caac80
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-27 21:25:53 +0800

    修剪字符串

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -4,19 +4,30 @@
 #include "stdafx.h"
 #include <windows.h>
 #include <wchar.h>
+#include <stdbool.h>
+#include "Shlwapi.h"
 
-#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "Shlwapi.lib")
 
 int wmain(void) {
 
-	SYSTEMTIME st = { 0 };
-	wchar_t buf[128] = { 0 };
+	wchar_t buf[] = L"23tennis74";
+	wchar_t trim[] = L"0123456789";
 
-	GetLocalTime(&st);
-	wsprintfW(buf, L"Today is %lu.%lu.%lu\n", st.wDay,
-		st.wMonth, st.wYear);
+	wprintf(L"Original string: %ls\n", buf);
 
-	wprintf(buf);
+	bool r = StrTrimW(buf, trim);
+
+	if (r == true) {
+
+		wprintf(L"The StrTrim() trimmed some characters\n", buf);
+	}
+	else {
+
+		wprintf(L"No characters were trimmed\n", buf);
+	}
+
+	wprintf(L"Trimmed string: %ls\n", buf);
 	getchar();
 	return 0;
 }
